d446ef3f54e32e48a9248b95d22680c251b2baf0
Ana Maria Horhoianu <ana_horhoianu@yahoo.com>
2021-06-01 17:31:24 +0100

    one commit

--- _data/news.js
+++ _data/news.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 async function getUser() {
     try {
-      const response = await axios.get(https://newsapi.org/v2/top-headlines?country=us&apiKey=03cb80608dda41b9bc00894d7349b8be);
-      console.log(response);
+      const response = await axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=03cb80608dda41b9bc00894d7349b8be");
+      return response.data;
     } catch (error) {
       console.error(error);
     }
