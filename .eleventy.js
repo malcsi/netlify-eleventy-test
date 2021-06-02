204f2d248e94d987e612e6d3d47944caaaf4a750
Ana Maria Horhoianu <ana_horhoianu@yahoo.com>
2021-06-02 13:56:40 +0100

    one commit

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,4 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("source");
         data: "_data"
-    eleventyConfig.setBrowserSyncConfig({
-        http: {
-            key: '/etc/localhost.key',
-            cert: '/etc/localhost.crt'
-        }
-    });
 };
