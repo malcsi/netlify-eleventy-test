82effd7964be1f727065d70dc8a126bb2ed1e8d7
Ana Maria Horhoianu <ana_horhoianu@yahoo.com>
2021-06-07 13:39:26 +0100

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("source");
+    eleventyConfig.addPassthroughCopy("css");
         data: "_data"
 };
