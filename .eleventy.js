eaf76cd5445b6d83de2d30b43f1f868f3fdc6c4a
Ana Maria Horhoianu <ana_horhoianu@yahoo.com>
2021-06-11 20:56:00 +0100

    feedback page updated

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy("source");
         data: "_data"
 };
